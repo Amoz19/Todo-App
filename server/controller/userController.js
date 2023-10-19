e879b09a2bd327f35cee73bf135126a64a596da9
Amoz19 <aungmon10co@gmail.com>
2023-10-19 18:59:37 +0630

    add:SomeUsefulPackages

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,18 +1,18 @@
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const User = require("../models/UserModel");
 
 const createUser = async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already Register");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
 
   try {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user?.password, salt);
     await user.save();
-    res.send(user);
+    res.send(_.pick(user, ["_id", "name", "email", "password"]));
   } catch (error) {
     res.json({ message: error.message });
   }
